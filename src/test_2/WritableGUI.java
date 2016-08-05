a2a2a1abc49b6886b1b6ccebbe8d79f08112e3d6
Titas Nandi <titas.nandi01@gmail.com>
2016-08-05 22:22:15 +0530

    Group Chat

---
+++ src/test_2/WritableGUI.java
@@ -0,0 +1,17 @@
+package test_2;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author titas
+ */
+public interface WritableGUI {
+    
+    void write(String s);
+    
+}
